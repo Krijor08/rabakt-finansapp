889748b9ccf0c293796118ef0a59b81a73a37b73
Krijor08 <krijor08@gmail.com>
2025-12-02 13:22:36 +0100

    Added a cors.go file and made a proper logger

--- go-backend/middleware/logger.go
+++ go-backend/middleware/logger.go
@@ -1,17 +1,27 @@
 package middleware
 
 import (
-	"log"
 	"net/http"
 	"time"
+
+	"github.com/rs/zerolog/log"
 )
 
-func Logger(next http.Handler) http.Handler {
+func Logger(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
-		next.ServeHTTP(w, r)
+		// Let the real handler run
+		handler.ServeHTTP(w, r)
 
-		log.Printf("%s %s %v", r.Method, r.URL.Path, time.Since(start))
+		// After the request finishes, log it
+		duration := time.Since(start)
+
+		log.Info().
+			Str("method", r.Method).    // GET, POST, etc.
+			Str("url", r.URL.String()). // /hello
+			Dur("took", duration).      // how long it took
+			Str("from", r.RemoteAddr).  // who made the request
+			Msg("request completed")
 	})
 }
