42e3527592919f467adbfa5479a30f277b017aa4
Krijor08 <krijor08@gmail.com>
2025-12-02 13:19:20 +0100

    Added /health and /ping routes

--- go-backend/main.go
+++ go-backend/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"go-backend/handlers"
 	"go-backend/middleware"
-	"log"
 	"net/http"
+
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
@@ -14,10 +15,19 @@ func main() {
 	wrappedRegister := middleware.Logger(registerHandler)
 	mux.Handle("/register", wrappedRegister)
 
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("pong"))
+	})
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Is good yes"))
+	})
 	loginHandler := http.HandlerFunc(handlers.Login)
 	wrappedLogin := middleware.Logger(loginHandler)
 	mux.Handle("/login", wrappedLogin)
 
-	log.Println("Server running on :45631")
-	log.Fatal(http.ListenAndServe(":45631", mux))
+	log.Info().Msg("Server running on port :5000")
+	http.ListenAndServe(":5000", middleware.CORS(mux))
 }
